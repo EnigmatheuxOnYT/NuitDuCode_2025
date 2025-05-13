c2b5b44f270e951db7f3445519ba094a03d24a50
Clem4096 <clem4097@gmail.com>
2025-05-13 20:53:30 +0200

    carré qui bouge

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ import pyxel
 class Main:
     def __init__(self):
         pyxel.init(width=256, height=256, title="Projet NDC", fps=60, quit_key=pyxel.KEY_Q,)
+        self.x=0
+
     
-    def update(self):pass
-    def draw(self):pass
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x,0,8,8,9)
+
     def run(self):pyxel.run(self.update,self.draw)
 
 if __name__ == '__main__':
