05798a75821b5fe235735570686d9367a8f776f5
Clem4096 <clem4097@gmail.com>
2025-05-13 20:47:39 +0200

    e

--- main.py
+++ main.py
@@ -3,4 +3,14 @@
 
 import pyxel
 
-pyxel.init(256, 256, title="Projet NDC")
+class Main:
+    def __init__(self):
+        pyxel.init(width=256, height=256, title="Projet NDC", fps=60, quit_key=pyxel.KEY_Q,)
+    
+    def update(self):pass
+    def draw(self):pass
+    def run(self):pyxel.run(self.update,self.draw)
+
+if __name__ == '__main__':
+    game = Main()
+    game.run()
