a98f5b8a0920501ce3df92ff0cccf94630e3287a
Clem4096 <clem4097@gmail.com>
2025-05-23 11:33:36 +0200

    Entités sprites ennemis

--- Jour J (laissser vide pour le moment)/main.py
+++ Jour J (laissser vide pour le moment)/main.py
@@ -5,19 +5,45 @@ class Sprite:
         self.img,self.x,self.y,self.width,self.height,self.colkey = img,x,y,w,h,colkey
     
     def draw(self,x,y):
-        px.blt(x,y,self.img,self.x,self.y,self.width,self.height,self.colkey,)
+        px.blt(x,y,self.img,self.x,self.y,self.width,self.height,self.colkey)
 
-class Player:
-    def __init__(self):
-        self.x,self.y=128,256
-        self.pos = (self.x,self.y)
-        self.maxpv = 5
-        self.pv = 5
-        self.spritepos = (0,0,0)
-        self.spritesize = (16,16)
+class Entity:
+    def __init__(self,x:int,y:int,w:int,h:int,maxpv:int,sprite:Sprite):
+        self.x,self.y=x,y
+        self.width,self.height = w,h
+        self.pv,self.maxpv = maxpv
+        self.sprite = sprite
     
+    @property
+    def pos(self):return (self.x,self.y)
+    @property
+    def size(self):return (self.width,self.height)
+
+    def move(self,modx,mody):
+        self.x+=modx
+        self.y+=mody
+    
+    def setpos (self,x,y):
+        self.y,self.y = x,y
+
     def draw(self):
-        px.bltm()
+        self.sprite.draw(self.x,self.y)
+
+
+class Player(Entity):
+    def __init__(self):
+        sprite = Sprite(0,0,0,self.width,self.height)
+        super().__init__(128,256,16,32,5,sprite)
+
+class Ennemi(Entity):
+    instances = []
+    def __init__ (self,maxpv,dmg,speed,sprite):
+        super().__init__(0,0,16,16,maxpv,sprite)
+        self.dmg = dmg
+        self.speed = speed
+        Ennemi.instances.append(self)
+        self.id = len(Ennemi.instances)
+
 
 class Main:
     def __init__(self):
