58f2343cd766c93ede20cb44e902aa3e08affd02
Clem4096 <clem4097@gmail.com>
2025-05-23 15:48:04 +0200

    obstacles et tt

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class Player(Entity):
     def __init__(self):
         self.width,self.height = 16,32
         sprite = Sprite(0,0,0,self.width,self.height)
-        super().__init__(128,224,16,32,5,sprite)
+        super().__init__(128,224,16,32,10,sprite)
         self.speed = 2
     
     def move(self,modx,mody):
@@ -63,6 +63,10 @@ class Player(Entity):
             if (not bullet.friendly) and self.check_collision(bullet):
                 self.damage(1)
                 bullet.destroy()
+        for obstacle in Obstacle.instances:
+            if self.check_collision(obstacle):
+                self.damage(1)
+                obstacle.destroy()
     
     def damage(self,amount):
         self.pv-=amount
@@ -128,30 +132,26 @@ ObstacleType((0,64))
 class Obstacle(Entity):
     instances = []
     y = -20
-    def __init__ (self,type:int,startx:tuple,resetround:bool=False):
-        if resetround:
-            Ennemy.y = -20
-        else:
-            Ennemy.y -= 70
+    def __init__ (self,type:int,startx:tuple):
         self.typeno = type
-        self.type:EnnemyType = EnnemyType.instances[type-1]
-        super().__init__(startx,Ennemy.y,16,16,self.type.maxpv,self.type.sprite)
-        Ennemy.instances.append(self)
-        self.speed = self.type.speed
-    
-    def damage (self,damage):
-        self.pv-=damage
-        if self.pv<=0:
-            self.destroy()
+        self.type:ObstacleType = ObstacleType.instances[type-1]
+        super().__init__(startx,Obstacle.y,16,16,1,self.type.sprite)
+        Obstacle.instances.append(self)
+        if Obstacle.y>-8000:
+            Obstacle.y-=80
     
     def destroy(self):
-        Ennemy.instances.remove(self)
+        Obstacle.instances.remove(self)
     
     def update(self):
         self.move(0,1)
+        if self.y>256:
+            Obstacle.instances.remove(self)
+            Obstacle(px.rndi(0,1),px.rndi(0,240))
 
 class Bullet(Entity):
     instances = []
+    speed = 3
     def __init__(self,friendly,pos):
         self.friendly = friendly
         sprite = Sprite(0,8,32,8,8)
@@ -160,9 +160,9 @@ class Bullet(Entity):
     
     def update (self):
         if self.friendly:
-            self.y-=3
+            self.y-=Bullet.speed
         else:
-            self.y+=3
+            self.y+=Bullet.speed
         if self.y>256 or self.y<-8:
             self.destroy()
     
@@ -193,7 +193,11 @@ class Explosion:
 
 vague1 = [Ennemy(px.rndi(1,3),px.rndi(0,240)) for _ in range(15)]+[Ennemy(px.rndi(1,3),px.rndi(0,240),True)]
 vague2 = [Ennemy(px.rndi(1,3),px.rndi(0,240)) for _ in range(30)]+[Ennemy(px.rndi(1,3),px.rndi(0,240),True)]
-vagues = [None,vague1,vague2]
+vague3 = [Ennemy(px.rndi(1,3),px.rndi(0,240)) for _ in range(45)]+[Ennemy(px.rndi(1,3),px.rndi(0,240),True)]
+vague4 = [Ennemy(px.rndi(1,3),px.rndi(0,240)) for _ in range(120)]+[Ennemy(px.rndi(1,3),px.rndi(0,240),True)]
+vagues = [None,vague1,vague2,vague3,vague4]
+
+obstacles = [Obstacle(px.rndi(0,1),px.rndi(0,240)) for _ in range(50)]
 
 class Main:
     def __init__(self):
@@ -235,10 +239,14 @@ class Main:
             bullet.update()
         for explosion in Explosion.instances:
             explosion.update()
+        for obstacle in Obstacle.instances:
+            obstacle.update()
         if len(Ennemy.instances) == 0:
             self.vagueno +=1
             Ennemy.instances = self.current_vague
             Ennemy.speedmod+=1
+            Bullet.speed +=1
+            self.player.pv+=5
     
     def init_waves(self):
         self.waves = []
@@ -273,9 +281,12 @@ class Main:
             bullet.draw()
         for explosion in Explosion.instances:
             explosion.draw()
+        for obstacle in Obstacle.instances:
+            obstacle.draw()
         self.player.draw()
-        px.text(0,0,"vies : "+str(self.player.pv),0)
-        px.text(0,10,"score : "+str(Ennemy.killed),0)
+        px.text(0,0,"Vies : "+str(self.player.pv),0)
+        px.text(0,10,"Score : "+str(Ennemy.killed),0)
+        px.text(0,10,"Round : "+str(self.vagueno),0)
 
 
     def run (self):
@@ -284,6 +295,7 @@ class Main:
         self.init_waves()
         px.run(self.update,self.draw)
         px.quit()
+        print("Score : "+Ennemy.killed)
 
 if __name__ == "__main__":
     Jeu = Main()
