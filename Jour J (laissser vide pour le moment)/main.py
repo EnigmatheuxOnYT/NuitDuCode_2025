99c47a931d2ac30d8d8dc4454cf6d59ee670a8bb
Clem4096 <clem4097@gmail.com>
2025-05-23 11:14:04 +0200

    sprites et player

--- Jour J (laissser vide pour le moment)/main.py
+++ Jour J (laissser vide pour le moment)/main.py
@@ -1,8 +1,27 @@
 import pyxel as px
 
+class Sprite:
+    def __init__(self,img,x,y,w=16,h=16,colkey=2):
+        self.img,self.x,self.y,self.width,self.height,self.colkey = img,x,y,w,h,colkey
+    
+    def draw(self,x,y):
+        px.blt(x,y,self.img,self.x,self.y,self.width,self.height,self.colkey,)
+
+class Player:
+    def __init__(self):
+        self.x,self.y=128,256
+        self.pos = (self.x,self.y)
+        self.maxpv = 5
+        self.pv = 5
+        self.spritepos = (0,0,0)
+        self.spritesize = (16,16)
+    
+    def draw(self):
+        px.bltm()
+
 class Main:
     def __init__(self):
-        px.load("theme.pyxres")
+        self.vague = 0
     
     def handle_input(self):
         pass
@@ -15,6 +34,7 @@ class Main:
 
     def run (self):
         px.init(256,256,title="Nom",fps=60,quit_key=px.KEY_Q)
+        px.load("theme.pyxres")
         px.run(self.update,self.draw)
         px.quit()
 
